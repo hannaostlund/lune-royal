7e4e32fdd35ae45a2ce38d9a08efc2d3d8288b43
Hanna Ostlund <143076946+hannaostlund@users.noreply.github.com>
2024-01-08 11:52:11 +0100

    set up reusable footer with JS
    using JavaScript to create a footer reusable for all pages

---
+++ javascript/footer.js
@@ -0,0 +1,28 @@
+// Reusable footer setup in JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+    const footerTemplate = document.createElement('footer');
+    footerTemplate.classList.add('site-footer');
+    footerTemplate.innerHTML = `
+        <div class="name-footer">
+            <h1 class="footer-h1">Lune Royal</h1>
+            <p class="footer-address">Stora Gatan 28 Västerås</p>
+            <p class="footer-text">Follow us on social media for the latest updates</p>
+        <div class="social-icons">
+            <a href="/">
+                <img src="/Images/linkedin.svg" alt="Linkedin icon" />
+            </a>     
+            <a href="/">
+                <img src="/images/githubicon.svg" alt="Github icon" />
+            </a>     
+        </div>
+        </div>
+
+    `;
+
+    const footerPlaceholder = document.getElementById('footer-content'); // Placeholder in HTML
+    if (footerPlaceholder) {
+        footerPlaceholder.appendChild(footerTemplate);
+    } else {
+        console.error('Footer placeholder element not found.');
+    }
+});
